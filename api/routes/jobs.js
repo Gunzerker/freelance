3e8202ba194aeb670f7de8002510d7dc6151601f
houssem <houssem.zerai@digit-u.com>
2021-04-15 12:22:53 +0100

    latest update

--- api/routes/jobs.js
+++ api/routes/jobs.js
@@ -57,7 +57,9 @@ router.post('/create_job',passport.authenticate("jwt", { session: false }),async
     const user = req.user;
     const {job_name,job_description,location_lat,location_lng} = req.body;
     const reverse_geocode = await axios.get(`https://api.openrouteservice.org/geocode/reverse?api_key=${config.openrouteservice_key}&point.lon=${location_lng}&point.lat=${location_lat}`);
+    console.log(reverse_geocode.data.features[0].properties)
     const queryString = "INSERT INTO `jobs`(`job_name`, `job_description`, `owner_user_id`,`location_lat`,`location_lng`,`date_creation`,`adress`) VALUES (?,?,?,?,?,?,?)";
+    //return res.send(reverse_geocode.data.features[0].properties)
     getConnection().query(queryString,[job_name,job_description,user.user_id,location_lat,location_lng,new Date().toISOString(),reverse_geocode.data.features[0].properties.label],(err,results,fields)=>{
         if(err){
             console.log("[ERROR]"+err)
@@ -85,7 +87,7 @@ router.post('/create_job',passport.authenticate("jwt", { session: false }),async
 
 router.get('/fetch_jobs',passport.authenticate("jwt", { session: false }),(req,res)=>{
 
-    const  queryString = "SELECT  j.* , u.user_name,u.user_last_name FROM jobs j join users u on j.owner_user_id = u.user_id order by job_id";
+    const  queryString = "SELECT  j.* , u.user_name,u.user_last_name,u.image_url FROM jobs j join users u on j.owner_user_id = u.user_id order by job_id";
     getConnection().query(queryString,[],(err,rows,fields)=>{
         if(err){
             console.log("[ERROR]"+err)
